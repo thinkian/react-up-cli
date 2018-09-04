fb67d4e4c0682c0d3ce25dadb2e49903e935ec1e
Ian McPhail <ian.mcphail@thinkcompany.com>
2018-09-04 09:57:21 -0400

    Fix issue with compose enhancer and Redux DevTools extension

--- templates/configureStore.js
+++ templates/configureStore.js
@@ -4,19 +4,17 @@ import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers';
 import rootSaga from './sagas';
 
-/* eslint-disable no-underscore-dangle */
-const devToolsExtension =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-/* eslint-enable */
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 
 const configureStore = preloadedState => {
   const sagaMiddleware = createSagaMiddleware();
-
-  const store = createStore(
-    rootReducer,
-    preloadedState,
-    compose(applyMiddleware(sagaMiddleware), devToolsExtension)
-  );
+  const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
+  const store = createStore(rootReducer, preloadedState, enhancer);
 
   sagaMiddleware.run(rootSaga);
 
